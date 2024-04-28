278cebdb00fbf9a43e5569e6df815cc8bc228415
AhmedAshraf44 <aa5485111@gmail.com>
2024-04-29 00:45:42 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/view/dash_board_view.dart';
+
+void main() {
+  runApp(const ResponsiveDashboard());
+}
+
+class ResponsiveDashboard extends StatelessWidget {
+  const ResponsiveDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: DashboardView(),
+    );
+  }
+}
+  
