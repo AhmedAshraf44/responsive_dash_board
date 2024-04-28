1cc87d7a7efe00d8ca1bdfd5746312a16380066e
AhmedAshraf44 <aa5485111@gmail.com>
2024-04-29 02:21:36 +0300

    adding images in assets

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+
+ import 'package:flutter/material.dart';
+
+const Color kPrimaryColor =  Color(0xff064061); 
+const Color kSecondColor =  Color(0xff4EB7F2); 
+const Color kWhiteColor =  Color(0xffFFFFFF); 
